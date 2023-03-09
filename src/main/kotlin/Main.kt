a8a1539a5bd1eeb1195c290a407a974953b405d5
chirchirmercy <cmercychptoo.com>
2023-03-09 08:58:54 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,56 @@
+fun main(){
+   println(names)
+    numbers(10,20,30,15)
+    people("john","faith","ann")
+
+}
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+
+fun name(name1:String,name2:){
+
+}
+
+
+
+
+
+
+//2. Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+
+fun numbers(num1:Int,num2:Int,num3:Int,num4:Int){
+    var numbers= arrayOf(num1,num2,num3,num4)
+    println(numbers.sortedArray())
+    println(numbers.average())
+    println(numbers.max())
+    println(numbers.min())
+
+}//3. Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+
+
+
+
+
+       // 4. Write and call a function that takes in an array of strings, joins them all into
+      //  one string and returns it. (4 pts)
+fun people(person1:String,person2:String,person3:String){
+    var people= arrayOf(person1,person2,person3)
+        println(people.sortedArray())
+           print(person1)
+           print(person2)
+           print(person3)
+           println()
+
+
+
+}
+
+
+
+
+
+
+
